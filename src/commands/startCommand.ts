adb34eba1ffdae2ae12b44b1d5f391a37be5a2e6
tjingsheng <tanjs41200@gmail.com>
2025-08-18 14:57:50 +0800

    fix start command

--- src/commands/startCommand.ts
+++ src/commands/startCommand.ts
@@ -1,5 +1,31 @@
+import { db } from "../db/index.ts";
+import { member } from "../db/schema.ts";
 import { type Middleware } from "../middlewares/botContex.ts";
 
 export const startCommand: Middleware = async (ctx) => {
-  await ctx.bot.sendMessage(ctx.chatId, "Hello from /start command");
+  const { bot, chatId } = ctx;
+
+  try {
+    await db
+      .insert(member)
+      .values({
+        chatId: String(chatId),
+        isAdmin: false,
+      })
+      .onConflictDoUpdate({
+        target: member.chatId,
+        set: { isAdmin: false },
+      });
+
+    await bot.sendMessage(
+      chatId,
+      "👋 Hello! You’ve been registered with the bot."
+    );
+  } catch (err) {
+    console.error("Error creating user in /start:", err);
+    await bot.sendMessage(
+      chatId,
+      "Something went wrong while registering you."
+    );
+  }
 };
