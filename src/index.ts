47de1215189c955e76972e835da616ae7829e56e
tjingsheng <tanjs41200@gmail.com>
2025-08-19 11:34:49 +0800

    remove test functionality

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
 import dotenv from "@dotenvx/dotenvx";
 import { registerStartFunctionality } from "./commands/start.ts";
-import { registerTestFunctionality, testCommand } from "./commands/test.ts";
 import { registerHiFunctionality } from "./commands/hi.ts";
 import { registerScheduleMessageFunctionality } from "./commands/schedule_message.ts";
 import { registerManageMessagesFunctionality } from "./commands/manage_messages.ts";
@@ -30,8 +29,6 @@ registerHiFunctionality(bot);
 registerScheduleMessageFunctionality(bot);
 
 registerStartFunctionality(bot);
-
-registerTestFunctionality(bot);
 
 registerManageMessagesFunctionality(bot);
 
